3a5a4c4ebc4000403dba4e7c28df7e2cdb44fdf8
Tan Shan Qi <shanqitan@gmail.com>
2026-01-10 17:57:25 +0800

    change address format

--- secondbook-frontend/src/pages/User/CheckoutPage.jsx
+++ secondbook-frontend/src/pages/User/CheckoutPage.jsx
@@ -14,7 +14,7 @@ const CheckoutPage = () => {
         fullName: '',
         phone: '',
         houseNo: '',    
-        address: '',    
+        street: '',    
         city: '',
         zip: '',        
         state: '',      
